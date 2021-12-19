983f0f948652d09f355cfc2597a6cb0c889ed8cd
Nick G <nick2123@gmail.com>
2021-12-19 14:12:10 -0800

    still working

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    gender: {},
+    looking_for: {},
+    age: {},
+    ethnicity: {},
+    education: {},
+    children: {},
+    description: {},
+    login_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "login",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
+
+module.exports = Project;
